1403ee595a55af39d3a4b9d3becd262304b9c91d
Alaa Alhazmi <alaaalhazmi@Alaas-MacBook-Pro.local>
2022-12-31 14:52:55 +0300

    update url

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-mumipanp.us-east-1.elasticbeanstalk.com:3000/api/v0",
+  apiHost: "http://udagram-api-dev.eba-mumipanp.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
