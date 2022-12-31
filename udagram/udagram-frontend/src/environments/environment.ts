d4a59fc5eef145b1911c54de4f8b29cfbbf9df85
Alaa Alhazmi <alaaalhazmi@Alaas-MacBook-Pro.local>
2022-12-31 13:50:34 +0300

    update url

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0'
+  apiHost: 'http://udagram-api-dev.eba-mumipanp.us-east-1.elasticbeanstalk.com:3000/api/v0'
 };
 
 /*
