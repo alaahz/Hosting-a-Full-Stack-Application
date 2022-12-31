d84bf0a75cf9e42a7290dac779e3163b7a60e7ca
Alaa Alhazmi <alaaalhazmi@Alaas-MacBook-Pro.local>
2022-12-31 13:15:50 +0300

    First commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,15 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.cjsd0yxjo24r.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export DB_PORT= 5432
+export PORT = 3000
+export AWS_BUCKET=arn:aws:s3:::udagrambucket-2022
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
+export AWS_ACCESS_KEY_ID = "AKIAZCDR5SICCPJYZSO6"
+export AWS_SECRET_ACCESS_KEY= "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"
